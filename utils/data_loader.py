76abf94dee647186fedc9ebc774971870abfe9c3
Egor <kamolikov.en@gmail.com>
2025-04-20 18:27:43 +0300

    Загрузка датасета: добавлен докстрнг

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -2,10 +2,18 @@ import pandas as pd
 import kagglehub
 from kagglehub import KaggleDatasetAdapter
 import streamlit as st
-import os
 
 @st.cache_data(show_spinner="Загружаем датасет с Kaggle...")
 def load_dataset() -> pd.DataFrame:
+    """
+    Загружает датасет с физическими и химическими свойствами органических веществ из Kaggle.
+
+    Эта функция использует библиотеку KaggleHub для загрузки датасета и возвращает его в виде DataFrame.
+    Датасет содержит информацию о различных органических веществах и их свойствах.
+
+    Возвращает:
+        pd.DataFrame: DataFrame с данными о физических и химических свойствах органических веществ.
+    """
     df = kagglehub.load_dataset(
         KaggleDatasetAdapter.PANDAS,
         "ivanyakovlevg/physical-and-chemical-properties-of-substances",
